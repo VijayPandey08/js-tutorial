6497b62f42a491227b10ec253d9135ef60d55387
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-05-12 11:24:12 +0000

    changes done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive
+// call by value
+// types: string, number, boolean, null,undefined, symbol, bigint
+
+const score = 100 
+
+// refrence (non-primitive)
+// call by refrence
+// types : arrays, objects, functions
+
+// java-script is dynamically typed, you do not need to define the data-type
+
+const heros = ["shaktiman","vijay","pandit"]  // array
+let myObj = {
+    face :"vijay",
+    age: 18,
+}    //object
+
+const myFunction = function(){
+    console.log("hello world");
+}
