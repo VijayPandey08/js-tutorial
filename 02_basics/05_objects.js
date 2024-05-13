f559dce510253527d701382d44b359fc115e98ec
VijayPandey08 <100491199+VijayPandey08@users.noreply.github.com>
2024-05-13 12:44:57 +0000

    json talks

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,24 @@
+const course ={
+    coursename: "js ",
+    price:"999",
+    courseinstructor:"vijay"
+}
+
+const {courseinstructor:instructor}  = course  // de-structure
+
+console.log(courseinstructor);
+console.log(instructor);
+
+//json
+// {
+//     "name":"vijay",
+//     "id":"1223",
+//     "price":"free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
